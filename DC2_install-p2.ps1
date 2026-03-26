dd6002bf93bacdabe8113d334914738ade93cefe
Dylan <dylanbourgeoispro@personalfile.me>
2026-03-26 14:11:48 +0100

    Add PowerShell script to promote DC2 to domain controller

---
+++ DC2_install-p2.ps1
@@ -0,0 +1,13 @@
+$NomDomaine = Read-Host "Entrez le nom du domaine à rejoindre (ex: tssr.lan)"
+
+Write-Host "Promotion du DC2..." -ForegroundColor Cyan
+
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+Write-Host "Veuillez entrer les identifiants de l'administrateur du domaine" -ForegroundColor Yellow
+$Credential = Get-Credential
+Install-ADDSDomainController `
+    -DomainName "$NomDomaine" `
+    -Credential $Credential `
+    -InstallDns:$true `
+    -Force:$true
