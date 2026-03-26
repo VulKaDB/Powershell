56bc5278bc691a9ba90db10c4607e9d24c81767b
Dylan <dylanbourgeoispro@personalfile.me>
2026-03-26 14:09:11 +0100

    Add PowerShell script for network configuration

---
+++ DC2_install.ps1
@@ -0,0 +1,12 @@
+# --- SAISIES UTILISATEUR ---
+$NomMachine = Read-Host "Nom de la Machine"
+$IPMachine  = Read-Host "IP de la Machine"
+$IP_DC1     = Read-Host "DNS de référence du DC1"
+$Passerelle = Read-Host "IP de la Paserelle"
+
+Write-Host "Configuration du Réseau" -ForegroundColor Cyan
+New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $IPMachine -PrefixLength 24 -DefaultGateway $Passerelle -ErrorAction SilentlyContinue
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $IP_DC1
+
+Write-Host "Rennomer la Machine" -ForegroundColor Yellow
+Rename-Computer -NewName $NomMachine -Force -Restart
